6e63df55bde20ae433f848805e12147946294755
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-10 16:52:42 +0900

    ファイル名修正(Post)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Todos from '../components/Todos'
+import Todos from '../components/Posts'
 import { useAuthContext } from '../context/AuthContext'
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
