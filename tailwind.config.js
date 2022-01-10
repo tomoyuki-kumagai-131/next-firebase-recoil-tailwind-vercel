d4c4e3d6fa066b182d088b7603781e9ce3418bf1
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-10 16:52:01 +0900

    設定変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
 	mode: "jit", //JITモードを有効化
 	// パージの対象ファイルを設定
 	purge: [
-		"./src/pages/**/*.{js,ts,jsx,tsx}",
-		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/pages/**/*.{js,jsx}",
+		"./src/components/**/*.{js,jsx}",
+		"./src/components/*.{js,jsx}",
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
