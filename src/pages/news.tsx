2957d850fa9088dfd6fe053759dbf71196e4f43c
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-20 01:41:11 +0900

    fix: infinite scroll, styles

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,18 +1,28 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import useSWR from 'swr'
+import { useState } from 'react';
+import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite'
 
-function fetcher(url) {
-  return fetch(url).then((res) => res.json())
-}
+
 
 function News() {
-  const { data, error } = useSWR('/api/news', fetcher)
+  // const { data, error } = useSWR('/api/news', fetcher)
+
+  function fetcher(url) {
+    return fetch(url).then((res) => res.json())
+  }
+
+  // const getKey = (index, previousPageData) => {
+  //   if(previousPageData && !previousPageData.length) return null;
+  //   return `https://newsapi.org/v2/top-headlines?country=jp&page=${index+1}&pageSize=3&&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+  // }
+  // const { data, error, size, setSize } = useSWRInfinite(getKey, fetcher)
+
+  const { data, error, size, setSize } = useSWRInfinite((index) => `https://newsapi.org/v2/top-headlines?country=jp&page=${index +1}&pageSize=3&&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`, fetcher)
 
   if (error) return <span>An error has occurred.</span>;
   if (!data) return <span className='flex items-center justify-center'>...Loading</span>;
-
-  const headline = data.data
 
   return (
     <div className='items-center justify-center text-center'>
@@ -21,18 +31,34 @@ function News() {
         更新
       </button>
       <div className='grid md:grid-cols-2 lg:grid-cols-3 lg:mx-6 xl:grid-cols-3'>
-        {headline.articles.map((item, i) => {
-            return (
-                <div key={i} className='mb-12 mx-8 text-left'>
-                  <div className=''>
-                  <Link href={item.url}>
-                    <span className='border-b'>{item.title}</span>
-                  </Link>
-                  <img src={item.urlToImage} className='mt-6 object-contain w-96'/>
+        {data.map((item) => {
+          return (
+            <>
+            {!data && '...loading'}
+              {item.articles.map((item, id) => {
+                return (
+                  <div key={id} className='mb-12 mx-8 text-left'>
+                    <div className=''>
+                    <Link href={item.url}>
+                      <span className='border-b'>{item.title}</span>
+                    </Link>
+                    <img src={item.urlToImage} className='mt-6 object-contain w-96'/>
+                    </div>
                   </div>
-                </div>
+                )
+              })}
+            </>
             )
-        })}
+          })}
+      </div>
+      <div className=''>
+        <button className='mb-12 border p-2 rounded-lg bg-orange-400 text-white outline-none' onClick={() => setSize(size + 1) }>
+          {size > 0 && data && typeof data[size - 1] === "undefined" ? (
+            'loading...'
+          ): (
+            '読み込む'
+          )}
+        </button>
       </div>
     </div>
   )
