8549e77fe7a9ac3b26e50cbed03b3d170c77d7d5
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-19 13:21:35 +0900

    add: news API作成

---
+++ src/pages/api/news.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async function (req, res) {
+	const url = await fetch(
+		`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+	);
+
+	const data = await url.json();
+
+	res.status(200).json({ data });
+}
