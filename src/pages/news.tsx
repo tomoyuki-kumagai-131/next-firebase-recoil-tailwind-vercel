f3f5b7b09298097302f31c22406e31a89189b2b4
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-19 16:03:35 +0900

    fix: design

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import useSWR from 'swr'
-import news from './api/news';
 
 function fetcher(url) {
   return fetch(url).then((res) => res.json())
@@ -21,7 +20,7 @@ function News() {
       <button className='mb-12 border p-2 rounded-lg bg-teal-500 text-white outline-none' onClick={()=> location.reload()}>
         更新
       </button>
-      <div className='grid lg:grid-cols-3 lg:mx-6 xl:grid-cols-3'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 lg:mx-6 xl:grid-cols-3'>
         {headline.articles.map((item, i) => {
             return (
                 <div key={i} className='mb-12 mx-8 text-left'>
