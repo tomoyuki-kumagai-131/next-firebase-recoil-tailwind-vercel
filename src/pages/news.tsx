38ceaf19dfeef2d7f0e1f8bcb9beb59a4efe2f16
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-19 15:50:28 +0900

    fix: /news img,link実装

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import useSWR from 'swr'
 import news from './api/news';
@@ -16,19 +17,24 @@ function News() {
 
   return (
     <div className='items-center justify-center text-center'>
-    <h1 className='mt-12 mb-8 text-2xl'>最新国内ニュース</h1>
-    <button className='mb-12 border p-2 rounded-lg bg-teal-500 text-white outline-none' onClick={()=> location.reload()}>
-      更新
-    </button>
-    {headline.articles.map((item, i) => {
-        return (
-          <div key={i} className='mb-12 mx-8'>
-            <Link href={item.url}>
-              <span className='border-b'>{item.title}</span>
-            </Link>
-          </div>
-        )
-      })}
+      <h1 className='mt-12 mb-8 text-2xl'>最新国内ニュース</h1>
+      <button className='mb-12 border p-2 rounded-lg bg-teal-500 text-white outline-none' onClick={()=> location.reload()}>
+        更新
+      </button>
+      <div className='grid lg:grid-cols-3 lg:mx-6 xl:grid-cols-3'>
+        {headline.articles.map((item, i) => {
+            return (
+                <div key={i} className='mb-12 mx-8 text-left'>
+                  <div className=''>
+                  <Link href={item.url}>
+                    <span className='border-b'>{item.title}</span>
+                  </Link>
+                  <img src={item.urlToImage} className='mt-6 object-contain w-96'/>
+                  </div>
+                </div>
+            )
+        })}
+      </div>
     </div>
   )
 }
