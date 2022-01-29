8d2fdc191f10c2a1fea16bc8b9d9a9441287364b
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-29 22:45:36 +0900

    add: ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,30 @@
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { RecoilRoot, useRecoilState } from "recoil";
-import { modalUpdate } from "../components/atoms/modalAtom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import ModalUpdate from "../components/ModalUpdate";
-import { AuthProvider } from "../context/AuthContext";
-import { db } from "../lib/firebase";
-import "../styles/globals.css";
+import { ChakraProvider } from '@chakra-ui/react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { RecoilRoot, useRecoilState } from 'recoil'
+import { modalUpdate } from '../components/atoms/modalAtom'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import ModalUpdate from '../components/ModalUpdate'
+import { AuthProvider } from '../context/AuthContext'
+import { db } from '../lib/firebase'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-
-	return (
-		<>
-			<AuthProvider>
-				<RecoilRoot>
-					<Header />
-					<Component {...pageProps} />
-					{/* <ModalUpdate /> */}
-					<Footer />
-				</RecoilRoot>
-			</AuthProvider>
-		</>
-	);
+  return (
+    <>
+      <AuthProvider>
+        <RecoilRoot>
+          <ChakraProvider>
+            <Header />
+            <Component {...pageProps} />
+            {/* <ModalUpdate /> */}
+            <Footer />
+          </ChakraProvider>
+        </RecoilRoot>
+      </AuthProvider>
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
