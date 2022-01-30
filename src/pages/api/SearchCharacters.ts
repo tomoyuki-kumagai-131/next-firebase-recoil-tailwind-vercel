3342ca10077edbbaf982f9c04204491ed35078c6
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-30 13:40:50 +0900

    add: GraphQL, 検索ロジック部分実装

---
+++ src/pages/api/SearchCharacters.ts
@@ -0,0 +1,59 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_URI,
+  cache: new InMemoryCache(),
+});
+
+export default async function SearchCharacters(req, res) {
+  const search = req.body;
+
+  try {
+    const { data } = await client.query({
+      query: gql`
+        query {
+          characters(filter: { name: "${search}" }) {
+            info {
+              count
+            }
+            results {
+              name
+              id
+              location {
+                name
+                id
+              }
+              image
+              origin {
+                name
+                id
+              }
+              episode {
+                id
+                episode
+                air_date
+              }
+            }
+          }
+        }
+      `,
+    });
+    res.status(200).json({
+      characters: data.characters.results,
+      error: null,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.message === '404: Not Found') {
+      res.status(404).json({
+        characters: null,
+        error: '該当のキャラクターは見つかりませんでした',
+      });
+    } else {
+      res.status(500).json({
+        characters: null,
+        error: 'サーバー側のエラーが発生しました。少し時間が経ってから再度お試しください。',
+      });
+    }
+  }
+}
