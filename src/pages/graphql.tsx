8bee02c56386604f0e2e1961414702d59f785461
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-30 13:40:25 +0900

    add: GraphQL クエリ検索機能実装

--- src/pages/graphql.tsx
+++ src/pages/graphql.tsx
@@ -1,34 +1,96 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
-import { IconButton, useToast } from '@chakra-ui/react'
-import { useState } from 'react'
-import Image from 'next/image'
-import { CloseIcon, SearchIcon } from '@chakra-ui/icons'
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import { Button, IconButton, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+import Image from 'next/image';
+import { CloseIcon, SearchIcon } from '@chakra-ui/icons';
+import { registerVersion } from 'firebase/app';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 function Graphql(results) {
-  const initialState = results
+  const initialState = results;
 
-  const [characters, setCharacters] = useState(initialState.characters)
+  const [characters, setCharacters] = useState(initialState.characters);
 
-  const [isLoading, setIsLoading] = useState()
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
 
-  const toast = useToast()
+  const [submitted, setSubmitted] = useState(false);
+
+  const toast = useToast();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const results = await fetch('/api/SearchCharacters', {
+      method: 'post',
+      body: search,
+    });
+    const { characters, error } = await results.json();
+
+    if (error) {
+      toast({
+        position: 'top',
+        title: 'エラーが発生しました',
+        description: error,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      setIsLoading(false);
+      toast({
+        position: 'top',
+        title: '読み込みが完了しました',
+        description: 'success',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      });
+      setCharacters(characters);
+    }
+  };
 
   return (
     <div className='text-center max-w-7xl mx-auto'>
-      <form className=''>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input
           onChange={(e) => setSearch(e.target.value)}
           value={search}
           className='border rounded-lg p-2 mt-4'
           placeholder='search word...'
         />
-        <CloseIcon w={4} h={4} onClick={() => setSearch('')} className='ml-3 cursor-pointer' />
-        <button type='submit' className='rounded-lg p-2 ml-1'>
-          <SearchIcon w={5} h={5} />
-        </button>
+        <CloseIcon
+          w={4}
+          h={4}
+          onClick={() => {
+            setSearch(''), setCharacters(initialState.characters);
+          }}
+          className='ml-3 cursor-pointer'
+        />
+        {isLoading ? (
+          <Button
+            isLoading
+            colorScheme='teal'
+            variant='solid'
+            type='submit'
+            className='rounded-lg ml-1'
+          >
+            検索
+          </Button>
+        ) : (
+          <Button
+            disabled={search === ''}
+            type='submit'
+            colorScheme='teal'
+            variant='solid'
+            className='rounded-lg ml-1'
+          >
+            検索
+          </Button>
+        )}
       </form>
+
       <div className='gap-4 grid-col-1 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
         {characters.map((character) => {
           return (
@@ -45,18 +107,18 @@ function Graphql(results) {
               </div>
               <span className='mb-3'>{character.name}</span>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_URI,
     cache: new InMemoryCache(),
-  })
+  });
 
   const { data } = await client.query({
     query: gql`
@@ -78,13 +140,13 @@ export async function getStaticProps() {
         }
       }
     `,
-  })
+  });
 
   return {
     props: {
       characters: data.characters.results,
     },
-  }
+  };
 }
 
-export default Graphql
+export default Graphql;
