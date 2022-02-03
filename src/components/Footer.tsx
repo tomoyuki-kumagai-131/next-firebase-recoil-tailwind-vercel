426daa4b020b201d09129101010489003ee857dd
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-02-03 09:45:46 +0900

    fix: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className='flex h-24 w-full items-center justify-center border-t p-4'>
+    <footer className='flex -mt-12 h-24 w-full items-center justify-center border-t p-4'>
       <a
         className='flex items-center justify-center'
         href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
@@ -9,14 +9,10 @@ function Footer() {
       >
         ©2021 Talexy All Rights Reserved.
         <br />
-        {/* <span className='ml-2'>Powered by </span> */}
         <img src='/vercel.svg' alt='Vercel Logo' className='h-4 ml-2' />
       </a>
     </footer>
-    // <span className="">
-    //   <p className="bg-gray-200 sticky p-4 text-center text-xs">Copyright © 2021 Talexy, Tomoyuki Kumagai, All Rights Reserved.</p>
-    // </span>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
